7dd836e143758028bf046b37c86490aef5961593
TheGreatA <nervous.fiend@gmail.com>
2018-02-07 16:12:39 +0330

    Now will give an error message when you bet with invalid accessToken

--- routes/bet.js
+++ routes/bet.js
@@ -8,22 +8,24 @@ bet.post('/bet', (req, res) => {
     let username = ''
     AccessToken.findOne({accessToken: accessToken}).exec(function (err, result) {
       if (err) return console.log(err)
-      if (!result) return console.log('Access Token not found')
-      username = result.user_id
-      Bet.makeBet(username, amount, condition, target, function (bet) {
-        if (bet === 'You don\'t have enough balance!') {
-          res.status(409).send('Insufficient funds')
-        } else if (bet) {
-          let resultBet = Object.assign({}, bet)
-          delete resultBet._doc._id
-          delete resultBet._doc.serverSeed
-          delete resultBet._doc.updatedAt
-          delete resultBet._doc.__v
-          res.status(200).json(resultBet._doc)
-        } else {
-          res.status(500).send('Bet Made Unsuccessfully')
-        }
-      })
+      if (!result) res.status(400).send('AccessToken is invalid')
+      else {
+        username = result.user_id
+        Bet.makeBet(username, amount, condition, target, function (bet) {
+          if (bet === 'You don\'t have enough balance!') {
+            res.status(409).send('Insufficient funds')
+          } else if (bet) {
+            let resultBet = Object.assign({}, bet)
+            delete resultBet._doc._id
+            delete resultBet._doc.serverSeed
+            delete resultBet._doc.updatedAt
+            delete resultBet._doc.__v
+            res.status(200).json(resultBet._doc)
+          } else {
+            res.status(500).send('Bet Made Unsuccessfully')
+          }
+        })
+      }
     })
   } else {
     res.send('Nothing Happened')
